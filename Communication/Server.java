921f395179758137480e53ded2f49abaa4ec488d
lirantul123 <93045864+lirantul123@users.noreply.github.com>
2024-01-07 15:52:57 +0200

    Update Server.java

--- Communication/Server.java
+++ Communication/Server.java
@@ -6,7 +6,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Server {
+public class Server{
     public static final int PORT = 8080;
 
     public static void main(String[] args) {
@@ -26,10 +26,15 @@ public class Server {
                 // Receive message
                 String receivedMessage = in.nextLine();
                 System.out.println("#: '" + receivedMessage + "'");
+                System.out.println(Client.t.print());
 
                 // Send message
-                System.out.print("!Enter a message to send: ");
+                System.out.print("Enter position(e.g 00): ");
                 String messageToSend = scanner.nextLine();
+
+                Client.t.setPlace(Integer.parseInt(messageToSend)/10, Integer.parseInt(messageToSend)%10, "O");
+                System.out.println(Client.t.print());
+
                 out.println(messageToSend);
 
                 System.out.print("(WAITING FOR FEEDBACK...)\n\n");
