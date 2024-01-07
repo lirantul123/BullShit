1b24bc611beba5bc103757d97cfa2a09b5b4881e
lirantul123 <93045864+lirantul123@users.noreply.github.com>
2024-01-07 15:52:34 +0200

    Update tictactow.java

--- Communication/tictactow.java
+++ Communication/tictactow.java
@@ -1,15 +1,35 @@
 public class tictactow {
+    public String[][] place;
 
-    public String print(){
+    public tictactow() {
+        place = new String[3][3];
+        initializeBoard();
+    }
+
+    private void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                place[i][j] = " ";
+            }
+        }
+    }
+
+    public String print() {
         String board = " _ _ _\n";
 
         for (int i = 0; i < 3; i++) {
             board += "|";
             for (int j = 0; j < 3; j++) {
-                board += "_|";
+                board += place[i][j] + "|";
             }
             board += "\n";
         }
+         board += " _ _ _\n";
+
         return board;
+    }
+
+    public void setPlace(int i, int j, String sym) {
+        place[i][j] = sym;
     }
 }
