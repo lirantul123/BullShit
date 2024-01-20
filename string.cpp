60cf6dc2791fc3b81106567acbc41130ab438fc2
liran <lira.tulchin@gmail.com>
2024-01-20 17:17:00 +0200

    e

--- string.cpp
+++ string.cpp
@@ -1,68 +1,92 @@
 #include <iostream>
-#include <vector>
+#include <stack>
+#include <cctype>
 
+bool isOperator(char ch);
+int precedence(char op);
+void applyOperator(std::stack<int>& values, std::stack<char>& operators, char op);
 
-template<typename T>
-class String
-{
-public:
-    String(const T* str) {
-        length = strlen(str);
-        elements.reserve(length);
-        for (int i = 0; i < length; ++i) {
-            elements.push_back(str[i]);
+int eval(const std::string& expression) {
+    std::stack<int> values;
+    std::stack<char> operators;
+
+    for (char ch : expression) {
+        if (std::isdigit(ch)) {
+            values.push(ch - '0');
+        } else if (isOperator(ch)) {
+            while (!operators.empty() && precedence(operators.top()) >= precedence(ch)) {
+                applyOperator(values, operators, operators.top());
+                operators.pop();
+            }
+            operators.push(ch);
         }
     }
 
-    ~String() {
+    while (!operators.empty()) {
+        applyOperator(values, operators, operators.top());
+        operators.pop();
     }
 
-    const std::vector<T>& getElements() const {
-        return elements;
-    }
-
-    String& operator+=(const T* str) {
-        int strLength = strlen(str);
-        elements.reserve(length + strLength);
-
-        for (int i = 0; i < strLength; ++i) {
-            elements.push_back(str[i]);
-        }
-
-        length += strLength;
-        return *this;
-    }
-    String& operator-=(const T* str) {
-        int strLength = strlen(str);
-        if (length >= strLength) {
-            elements.erase(elements.end() - strLength, elements.end());
-            length -= strLength;
-        }
-        else {
-            elements.clear();
-            length = 0;
-        }
-        return *this;
-    }
-
-private:
-    int length;
-    std::vector<T> elements;
-};
-
-
-int main()
-{
-    String<char> name("yogin");
-    name += "moshi";
-    name += "choen";
-    name -= "moshi";// deletes by number and not by characters
-
-    for (auto& let : name.getElements())
-    {
-        std::cout << let << "";
-    }
-    std::cout << "\n";
-
+    return values.top();
 }
 
+bool isOperator(char ch) {
+    return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^';
+}
+
+int precedence(char op) {
+    switch (op) {
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+            return 2;
+        case '^':
+            return 3;
+        default:
+            return 0;
+    }
+}
+
+void applyOperator(std::stack<int>& values, std::stack<char>& operators, char op) {
+    int operand2 = values.top();
+    values.pop();
+    int operand1 = values.top();
+    values.pop();
+
+    switch (op) {
+        case '+':
+            values.push(operand1 + operand2);
+            break;
+        case '-':
+            values.push(operand1 - operand2);
+            break;
+        case '*':
+            values.push(operand1 * operand2);
+            break;
+        case '/':
+            values.push(operand1 / operand2);
+            break;
+        case '^':
+            int ans=0;
+            for (int i =0; i < operand2; i++) { ans += operand1*operand1; }
+            values.push(ans);
+            break;
+
+    }
+
+    std::cout << operand1 << ' ' << op << ' ' << operand2 << " = " << values.top() << std::endl;
+}
+
+int main() {
+    std::string expression;
+    std::cout << "Enter an arithmetic expression: ";
+    std::getline(std::cin, expression);
+    int result = eval(expression);
+    
+    std::cout << "\n";
+    std::cout << expression << " = " << result << std::endl;
+
+    return 0;
+}
